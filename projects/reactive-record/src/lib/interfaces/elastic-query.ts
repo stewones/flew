df52724c96a3d98bac917d6ad22b32899f8ccae8
Stewan <oi@stewan.io>
2018-10-12 11:51:38 -0300

    fix(): make elastic query more flexible until add a better typing

--- projects/reactive-record/src/lib/interfaces/elastic-query.ts
+++ projects/reactive-record/src/lib/interfaces/elastic-query.ts
@@ -9,17 +9,19 @@ export interface ElasticQuery {
     sort?: any,                         // eg: { "created_at": { "order": "desc" } }
     aggs?: any,                         // see elastic aggs docs
     range?: any,                        // see elastic range docs
+    bool?: any,                         // see elastic bool docs
     nested?: {
         path: string,
         score_mode?: string,            // eg: avg
         query: ElasticQuery
     },
-    bool?: {
-        must: ElasticMatch | ElasticMatch[],
-        must_not: ElasticMatch | ElasticMatch[],
-        filter: ElasticMatch,
-        should: ElasticMatch | ElasticMatch[],
-        minimum_should_match?: number,  // eg: 1
-        boost: number                   // eg: 1.0
-    }
+    // @todo better typing
+    // bool?: {
+    //     must: ElasticMatch | ElasticMatch[],
+    //     must_not: ElasticMatch | ElasticMatch[],
+    //     filter: ElasticMatch,
+    //     should: ElasticMatch | ElasticMatch[],
+    //     minimum_should_match?: number,  // eg: 1
+    //     boost: number                   // eg: 1.0
+    // }
 }
