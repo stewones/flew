c40a31e2be372d12f1e3babb9adcc6d9c7496478
Stewan <oi@stewan.io>
2019-06-21 17:35:48 -0300

    fix(): edge case on deploy script and add travis to the newer packages

--- extra/releaser/release-patch.sh
+++ extra/releaser/release-patch.sh
@@ -4,21 +4,37 @@ npm run lint:core
 npm run lint:rr
 npm run lint:state
 npm run lint:firebase
+
 npm run test:angular
 npm run test:ionic
 npm run test:core
 npm run test:rr
 npm run test:state
 npm run test:firebase
+
+# -- BUILD -- ##
 npm run build:angular
 npm run build:ionic
 npm run build:core
 npm run build:rr
 npm run build:state
 npm run build:firebase
+# -- /BUILD -- ##
+
+# -- BUMP -- ##
 npm run bump:patch
 npm run changelog:patch
-npm run deploy:copy
+# -- /BUMP -- ##
+
+# -- REBUILD -- ##
+npm run build:angular
+npm run build:ionic
+npm run build:core
+npm run build:rr
+npm run build:state
+npm run build:firebase
+# -- /REBUILD -- ##
+
 npm run deploy:git
 npm run deploy:npm
 npm run deploy:ci
