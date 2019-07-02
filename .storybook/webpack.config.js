376dd65715b6b351ea003cc137d60cd4260e6508
Stewan <oi@stewan.io>
2019-07-02 00:19:59 -0300

    feat(storybook): add storysource plugin

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /.story.ts$/,
+    loaders: [
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' }
+      }
+    ],
+    enforce: 'pre',
+    include: require('path').resolve(__dirname, '../apps/app')
+  });
+  if (process.env.NODE_ENV !== `test`) {
+    config.node = { fs: 'empty' };
+  }
+  return config;
+};
