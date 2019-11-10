ac1436c1be6c08a922b4d47479fe910045e644a0
Stewan <oi@stewan.io>
2019-11-10 12:26:05 -0300

    chore(): remove test app

--- apps/rr/demo/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'rr-demo',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/rr/demo/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
