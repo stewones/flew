42bc0afe3af212d971d4f6f760091fe2f29115bb
Stewan <oi@stewan.io>
2019-11-22 16:53:07 -0300

    chore(schematics): attempt to fix build

--- tools/copy.sh
+++ tools/copy.sh
@@ -1,10 +1,12 @@
 cp -rf ./tools/schematics/collection/files ./dist/libs/schematics/collection
 cp -rf ./tools/schematics/container/files ./dist/libs/schematics/container
 cp -rf ./tools/schematics/component/files ./dist/libs/schematics/component
+cp -rf ./tools/schematics/page/files ./dist/libs/schematics/page
 
 cp -rf ./tools/schematics/collection/schema.json ./dist/libs/schematics/collection
 cp -rf ./tools/schematics/container/schema.json ./dist/libs/schematics/container
 cp -rf ./tools/schematics/component/schema.json ./dist/libs/schematics/component
+cp -rf ./tools/schematics/page/schema.json ./dist/libs/schematics/page
 
 cp -rf ./libs/schematics/README.md ./dist/libs/schematics
 cp -rf ./libs/schematics/package.json ./dist/libs/schematics
