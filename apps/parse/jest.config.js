d09d984037476d954e60c9b27ff4c475374812c4
Stewan <oi@stewan.io>
2019-12-01 22:33:02 -0300

    chore()

--- apps/parse/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'rr-parse',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/rr/parse',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
