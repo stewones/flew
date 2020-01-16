a6b8bd0db154952d2747b57d5b514cf3d09862f9
Stewan <oi@stewan.io>
2020-01-16 18:54:17 -0300

    chore(): add include example to parse demo

--- apps/parse-demo/src/app/app.component.ts
+++ apps/parse-demo/src/app/app.component.ts
@@ -10,6 +10,27 @@ export class AppComponent implements OnInit {
   title = 'parse-demo';
 
   ngOnInit() {
+    //this.exerciseTest()
+    this.includeTest();
+  }
+
+  includeTest() {
+    collection(`Order`)
+      .driver('parse')
+      // .include([`parts`, `requester`, `reviewer`])
+      .query({
+        include: [`parts`, `parts.vendor`, `requester`, `reviewer`],
+        matches: () => [`code`, `O-410`]
+      })
+
+      .findOne()
+      //.pipe(map(entries => (entries ? entries[0] : {})))
+      .subscribe(order => {
+        console.log(`item`, order);
+      });
+  }
+
+  exerciseTest() {
     console.log('start testing');
 
     //
