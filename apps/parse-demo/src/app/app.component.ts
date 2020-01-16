0e37e54588ec3dc4b5e3645980c82341a5e08268
Matheus Davidson <matheusdavidson@gmail.com>
2020-01-15 21:21:35 -0300

    chore(parse/demo): example query

--- apps/parse-demo/src/app/app.component.ts
+++ apps/parse-demo/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { collection } from '@reative/core';
 
 @Component({
   selector: 'reative-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'parse-demo';
+
+  ngOnInit() {
+    console.log('start testing');
+
+    //
+    // Query example
+    collection('Exercise')
+      .driver('parse')
+      .query({
+        or: [
+          {
+            and: [
+              {
+                equalTo: () => ['hasProgram', true]
+              }
+            ]
+          },
+          {
+            and: [
+              {
+                equalTo: () => ['hasProgram', false]
+              }
+            ]
+          }
+        ]
+      })
+      .count()
+      .subscribe(entries => console.log('entries', entries));
+  }
 }
