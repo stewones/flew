5a00bdcfd290f4c1c80a56255462f8deae2e0a0e
Stewan <oi@stewan.io>
2020-02-03 21:05:15 -0300

    fix(http): worker body

--- libs/core/worker/http.js
+++ libs/core/worker/http.js
@@ -1,10 +1,16 @@
-onmessage = payload => {
-  const data = payload.data || {};
+onmessage = req => {
+  const data = req.data || {};
   const url = data.url;
   const method = data.method || 'get';
   const body = data.body;
+
   let headers = {
     ...data.headers
+  };
+
+  let payload = {
+    method: method,
+    headers: headers
   };
 
   if (method !== 'get') {
@@ -13,13 +19,14 @@ onmessage = payload => {
       Accept: 'application/json, text/plain',
       'Content-Type': 'application/json;charset=UTF-8'
     };
+    payload = {
+      method: method,
+      headers: headers,
+      body: JSON.stringify(body)
+    };
   }
 
-  fetch(url, {
-    method: method,
-    headers: headers,
-    body: JSON.stringify(body)
-  }).then(response => {
+  fetch(url, payload).then(response => {
     if (response.status >= 200 && response.status < 300) {
       response.json().then(it => postMessage(it));
     } else {
