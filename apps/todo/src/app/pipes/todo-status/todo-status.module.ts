e417b8c8674cae60ce2423dc18e4d97ac748b340
Stewan <oi@stewan.io>
2020-05-02 17:57:01 -0300

    chore(): remove old todo app example

--- apps/todo/src/app/pipes/todo-status/todo-status.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TodoStatusPipe } from './todo-status.pipe';
-@NgModule({
-  declarations: [TodoStatusPipe],
-  imports: [],
-  exports: [TodoStatusPipe]
-})
-export class TodoStatusPipeModule {}
