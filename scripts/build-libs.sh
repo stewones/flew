9add79604614ff4a3cca6643c3b1a78d9685a597
Stewan <oi@stewan.io>
2020-05-05 13:02:21 -0300

    chore(): add build-libs scripts needed to publish libs

---
+++ scripts/build-libs.sh
@@ -0,0 +1,16 @@
+ng build core
+npm install ./dist/libs/core
+ng build cache
+npm install ./dist/libs/cache
+ng build firebase
+npm install ./dist/libs/firebase
+ng build parse
+npm install ./dist/libs/parse
+ng build state
+npm install ./dist/libs/state
+ng build angular
+npm install ./dist/libs/angular
+
+
+
+
