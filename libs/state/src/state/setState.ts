f1e8d3e2ccf57d57869a5be3af2d4865d329ff56
Stewan <oi@stewan.io>
2020-05-06 00:26:15 -0300

    chore(setState): there's no path in here

--- libs/state/src/state/setState.ts
+++ libs/state/src/state/setState.ts
@@ -4,23 +4,23 @@ import { dispatch } from '../store/dispatch';
 import { Reative } from '@reative/core';
 
 export function setState(
-  path: string,
+  key: string,
   value: any,
   options: SetStateOptions = { save: true }
 ) {
   options = { ...defaultStateOptions, ...options };
   const currentState = store().getState();
-  const nextState = { ...currentState, [path]: value };
+  const nextState = { ...currentState, [key]: value };
 
   dispatch({
     type: 'MEMO_UPDATE',
-    path: path,
+    path: key,
     payload: value
   });
 
   if (Reative.storage && options.save) {
     try {
-      Reative.storage.set(path, nextState);
+      Reative.storage.set(key, nextState);
     } catch (err) {}
   }
 }
