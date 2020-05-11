3765e9422c2953712d2fea702da4834e7739aca9
Stewan <oi@stewan.io>
2020-05-11 13:23:31 -0300

    chore(): build libs

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -11,6 +11,16 @@ npm install ./dist/libs/state
 ng build angular
 npm install ./dist/libs/angular
 
+sudo chown -R $USER ./dist/libs/angular
+sudo chown -R $USER ./dist/libs/state
+sudo chown -R $USER ./dist/libs/parse
+sudo chown -R $USER ./dist/libs/firebase
+sudo chown -R $USER ./dist/libs/cache
+sudo chown -R $USER ./dist/libs/core
 
-
-
+sudo chmod +x ./dist/libs/angular
+sudo chmod +x ./dist/libs/state
+sudo chmod +x ./dist/libs/parse
+sudo chmod +x ./dist/libs/firebase
+sudo chmod +x ./dist/libs/cache
+sudo chmod +x ./dist/libs/core
