255068bf961a407435b86ac41eafb3c88ba32d37
Stewan <oi@stewan.io>
2020-05-21 19:40:46 -0300

    chore(): add boolean example to counter app

--- apps/counter/src/app/app.component.ts
+++ apps/counter/src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { select, setState, getState } from '@reative/state';
 @Component({
   selector: 'reative-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   display$ = select<number>('counter');
   displayTree$ = select<{ total: number }>('counter-tree');
+  displayBoolean$ = select<boolean>('boolean-item');
 
   constructor() {}
 
   ngOnInit() {
     setState(`counter`, 420);
     setState(`counter-tree`, { total: 210 });
+    setState(`boolean-item`, false);
   }
 
   increment() {
@@ -31,5 +33,13 @@ export class AppComponent implements OnInit {
 
   decrementTree() {
     setState(`counter-tree`, { total: getState(`counter-tree`).total - 1 });
+  }
+
+  active() {
+    setState(`boolean-item`, true);
+  }
+
+  deactive() {
+    setState(`boolean-item`, false);
   }
 }
