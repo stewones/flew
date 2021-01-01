a0cab7a5075f3644d436c46e76afb449df9575ca
Stewan <oi@stewan.io>
2020-12-31 22:07:44 -0300

    fix(state): immer produce needs to differentiate objects

--- projects/libs/state/src/store/createReducer.ts
+++ projects/libs/state/src/store/createReducer.ts
@@ -2,7 +2,12 @@ import produce from 'immer';
 export function createReducer<T = any>(init: T, tree) {
   return function(state: T = init, action: { type: string; payload: any }) {
     if (tree[action.type]) {
-      return produce(state, draft => tree[action.type](draft, action));
+      return produce(state, draft => {
+        if (typeof state === 'object') {
+          return void tree[action.type](draft, action);
+        }
+        return tree[action.type](draft, action);
+      });
     }
     return state;
   };
