3169dabdefbe2cf4ecce5e2451efe85319edcccc
Stewan <oi@stewan.io>
2021-01-06 20:08:06 -0300

    chore(state): fix exports

--- projects/libs/state/src/index.ts
+++ projects/libs/state/src/index.ts
@@ -1,15 +1,2 @@
-// store
-export * from './store/createStore';
-export * from './store/createAction';
-export * from './store/createReducer';
-export * from './store/dispatch';
-export * from './store/store';
-export * from './store/connect';
-export * from './store/applyDevTools';
-
-// state
-export * from './state/getState';
-export * from './state/install';
-export * from './state/resetState';
-export * from './state/setState';
-export * from './state/unsetState';
+export * from './store';
+export * from './state';
---
+++ projects/libs/state/src/store/index.ts
@@ -0,0 +1,7 @@
+export * from './applyDevTools';
+export * from './connect';
+export * from './createAction';
+export * from './createReducer';
+export * from './createStore';
+export * from './dispatch';
+export * from './store';
