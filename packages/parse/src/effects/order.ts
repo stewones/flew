687b0362d9d6261f9ccb5cb85df4f9388e186445
stewones <oi@stewan.io>
2021-12-16 16:43:25 -0300

    fix: parse query sort

--- packages/parse/src/effects/order.ts
+++ packages/parse/src/effects/order.ts
@@ -8,29 +8,37 @@ import { isArray, isEmpty, isObject } from 'lodash';
  * @param {*} connector
  */
 export function order(sort: any, connector: any) {
+  const asc = [];
+  const desc = [];
+
   if (isArray(sort)) {
-    // this.log().success()(`parse sort array -> ${sort}`);
     sort.map(s => {
       if (isEmpty(s)) throw new Error(`sort object in array can't be null`);
       for (const k in s) {
         if (s[k] === 'asc') {
-          connector.ascending(k);
+          asc.push(k);
         }
         if (s[k] === 'desc') {
-          connector.descending(k);
+          desc.push(k);
         }
       }
     });
   } else if (isObject(sort)) {
-    // this.log().success()(`parse sort object -> ${JSON.stringify(sort)}`);
     if (isEmpty(sort)) throw new Error(`sort object can't be null`);
     for (const k in sort) {
       if (sort[k] === 'asc') {
-        connector.ascending(k);
+        asc.push(k);
       }
       if (sort[k] === 'desc') {
-        connector.descending(k);
+        desc.push(k);
       }
     }
+  }
+
+  if (asc.length > 0) {
+    connector.addAscending(asc.join(','));
+  }
+  if (desc.length > 0) {
+    connector.addDescending(desc.join(','));
   }
 }
