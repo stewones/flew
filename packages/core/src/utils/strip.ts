4ec4f24199a891d63bb5108c19dc04bf320e7d2a
stewones <oi@stewan.io>
2022-06-24 14:56:52 -0300

    fix: stripEmoji effect

--- packages/core/src/utils/strip.ts
+++ packages/core/src/utils/strip.ts
@@ -1,8 +1,8 @@
 import { isString } from 'lodash';
 
-export function stripEmoji(str) {
-  if (!isString(str)) return;
-  return str
+export function stripEmoji(data) {
+  if (!isString(data)) return data;
+  return data
     .replace(
       /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
       '',
